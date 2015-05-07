745a8c4276361c82928009ee578a6d2e4c0458ed
Diego Armijo <diego@blvr.com>
2015-05-07 13:47:15 -0700

    Initial Site Files

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+    $('.lead-content').backstretch("/assets/images/header.jpg");
+    $('.money-master').backstretch("/assets/images/robbins.jpg");
+    $('.video').backstretch("/assets/images/video.jpg");
+});
