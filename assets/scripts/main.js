eb064be000225785f3b88df8d6f89b1fb4e5c21c
Diego Armijo <diego@blvr.com>
2015-05-08 16:42:10 -0700

    added animations and video modal

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,4 +6,23 @@ $(document).ready(function(){
             $(target).backstretch(image);
         });
     }
+
+    new WOW().init();
+
+    function centerModal() {
+        $(this).css('display', 'block');
+        var $dialog = $(this).find(".modal-dialog");
+        var offset = ($(window).height() - $dialog.height()) / 2;
+        // Center modal vertically in window
+        $dialog.css("margin-top", offset);
+    }
+
+    $('#myModal').on('show.bs.modal', centerModal);
+    $(window).on("resize", function () {
+        $('.modal:visible').each(centerModal);
+    });
+
+    $("#myModal").on('hidden.bs.modal', function (e) {
+        $("#myModal iframe").attr("src", $("#myModal iframe").attr("src"));
+    });
 });
